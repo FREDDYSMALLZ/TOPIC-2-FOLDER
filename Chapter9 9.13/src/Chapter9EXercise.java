973795b5c273931caac2f7c2ba613fe85af67603
Fredrick Ondieki <freddysmallz@gmail.com>
2017-02-12 17:04:06 -0600

    Update Chapter9EXercise.java

--- Chapter9 9.13/src/Chapter9EXercise.java
+++ Chapter9 9.13/src/Chapter9EXercise.java
@@ -7,17 +7,6 @@ public class Chapter9EXercise {
         System.out.print("Enter the number of rows and columns in the array: ");
         int row = input.nextInt();
         int column = input.nextInt();
-        double[][] m = new double[row][column];
-
-        System.out.println("Enter the array:");
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[i].length; j++) {
-                m[i][j] = input.nextDouble();
-            }
-        }
-
-        
-
        
 	}
 
