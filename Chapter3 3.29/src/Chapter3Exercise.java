0763d72a1084df4c8334d8e4453148948b8f1753
Fredrick Ondieki <freddysmallz@gmail.com>
2017-02-12 15:58:59 -0600

    Update Chapter3Exercise.java
    second commit

--- Chapter3 3.29/src/Chapter3Exercise.java
+++ Chapter3 3.29/src/Chapter3Exercise.java
@@ -12,27 +12,29 @@ public class Chapter3Exercise {
 		  double y1 = input.nextDouble();
 		  double r1 = input.nextDouble();
 		 
-		  System.out
+	System.out
 		    .print("Enter circle1's center x-, y-coordinates, and radius:");
 		  double x2 = input.nextDouble();
 		  double y2 = input.nextDouble();
 		  double r2 = input.nextDouble();
+		  
+//circle2 is inside circle1 if the distance between the centers,<=r1 - r2
+//circle2 overlaps circle1 if the distance between the centers,<=r1 + r2		  
+		  double d = ((x2 - x1)* 2) + ((y2 - y1)* 2);
 		 
-		  double d = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		 
-		  if (d <= Math.abs(r2 - r1)) {
+		  if (d <= (r2 - r1)) {
 		   if (r1 > r2) {
 		    System.out.print("circle2 is inside circle1");
 		   } else if (r2 > r1) {
 		    System.out.print("circle1 is inside circle2");
 		   } else {
-		    System.out.print("circle2 is indentical to circle1");
+		    System.out.print("circle2 is the same as circle1");
 		   }
 		  } else if (d < r2 + r1) {
 		   System.out.print("circle2 overlaps circle1 ");
 		  } else if (d >= r2 + r1) {
-		   System.out.print("circle2 does not overlaps circle1 ");
-
+		   System.out.print("circle2 does not overlap circle1 ");
+	  
 	}
 
 }
