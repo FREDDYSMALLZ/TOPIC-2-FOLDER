c7599647022540dea529170db28d7f3627992eba
Fredrick Ondieki <freddysmallz@gmail.com>
2017-02-12 03:03:46 -0600

    Update Chapter3Exercise.java

--- Chapter3 3.2/src/Chapter3Exercise.java
+++ Chapter3 3.2/src/Chapter3Exercise.java
@@ -4,20 +4,20 @@ public class Chapter3Exercise {
 	public static void main(String[] args) {
 		// Game, adding three numbers
 		
-		int number1 = (int) (System.currentTimeMillis() % 10);
-		  int number2 = (int) (System.currentTimeMillis() * 7 % 10);
-		  int number3 = (int) (System.currentTimeMillis() * 13 % 10);
+		int number1 = (int) (System.currentTimeMillis() % 10);//generates number1
+		  int number2 = (int) (System.currentTimeMillis() * 7 % 10);//generates number2
+		  int number3 = (int) (System.currentTimeMillis() * 13 % 10);//generates number3
 		  
 		  // Creating  a Scanner
 		  Scanner input = new Scanner(System.in);
 		 
 		  System.out.print("What is " + number1 + " + " + number2 + " + "
-		    + number3 + "? ");
+		    + number3 + "? "); // Showing the question
 		 
 		  int answer = input.nextInt();
 		 
 		  System.out.println(number1 + " + " + number2 + " + " + number3 + " = " + answer
-		    + " is " + (number1 + number2 + number3 == answer));
+		    + " is " + (number1 + number2 + number3 == answer)); // Display results
 
 	}
 
