19a303b00403babd644e9e6cebff388c80f458f6
Fredrick Ondieki <freddysmallz@gmail.com>
2017-02-12 16:04:26 -0600

    Update Chapter3Exercise.java
    second commit after making a few changes

--- Chapter3 3.3/src/Chapter3Exercise.java
+++ Chapter3 3.3/src/Chapter3Exercise.java
@@ -13,18 +13,24 @@ public class Chapter3Exercise {
 		  double d = input.nextDouble();
 		  double e = input.nextDouble();
 		  double f = input.nextDouble();
+		   
 		 // Display to the console if the equation has no solution
+		
 		  if (a * d - b * c == 0) {
 		   System.out.println("The equation has no solution.");
-		   System.exit(0);
+		   
 		  }
-		 
+		  
+		 // Using cramer's rule
+		  
 		  double x = (e * d - b * f) / (a * d - b * c);
 		  double y = (a * f - e * c) / (a * d - b * c);
 		  
 		 //Display solution to the console
+		  
 		  System.out.print("x is " + x);
 		  System.out.println(" and y is " + y);
+
 
 	}
 
