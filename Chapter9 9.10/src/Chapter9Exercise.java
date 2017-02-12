d4375a930cd4b5e24f2c17c4fc328edfa8546cb6
Fredrick Ondieki <freddysmallz@gmail.com>
2017-02-12 17:02:02 -0600

    Update Chapter9Exercise.java
    second commit

--- Chapter9 9.10/src/Chapter9Exercise.java
+++ Chapter9 9.10/src/Chapter9Exercise.java
@@ -3,6 +3,8 @@ public class Chapter9Exercise {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Algebra;QuadraticEquation
+		
 		Scanner input = new Scanner(System.in);
 	    System.out.print("Enter a, b, c: ");
 	    double a = input.nextDouble();
@@ -17,56 +19,58 @@ public class Chapter9Exercise {
 	    }
 	    else if (discriminant == 0)
 	    {
-	      System.out.println("The root is " + equation.getRoot1());
+	      System.out.println("The root is " + equation.getRoot1());// display one root
 	    }
-	    else // (discriminant >= 0)
+	    else //(discriminant >0)
 	    {
-	      System.out.println("The roots are " + equation.getRoot1() 
-	        + " and " + equation.getRoot2());
+	    	System.out.println("The roots are "+ equation.getRoot1() + " and " 
+	    + equation.getRoot2());
+	    }
+	    
 	    }  
 	  }
-	}
+	
 
 	class QuadraticEquation {
 	  private double a;
 	  private double b;
 	  private double c;
 
-	  public QuadraticEquation(double newA, double newB, double newC) {
-	    a = newA;
-	    b = newB;
-	    c = newC; 
+	  public QuadraticEquation(double x, double y, double z) {
+	    a = x;
+	    b = y;
+	    c = z; 
 	  }
 	  
-	  double getA() {
+	  double getx() {
 	    return a;
 	  }
 
-	  double getB() {
+	  double gety() {
 	    return b;
 	  }
 
-	  double getC() {
+	  double getz() {
 	    return c;
 	  }
 
-	  double getDiscriminant() {
+	  double getDiscriminant() { //Method used to returns the discriminant
 	    return b * b - 4 * a * c;
 	  }
 
-	  double getRoot1() {
+	  double getRoot1() {// Method to return the root of the first equation
 	    if (getDiscriminant() < 0)
 	      return 0;
 	    else {
-	      return (-b + getDiscriminant()) / (2 * a);
+	      return (-b + getDiscriminant()) / (2 * a);// root equation r1
 	    }
 	  }
 
-	  double getRoot2() {
+	  double getRoot2() {//Method to return the root of the second equation
 	    if (getDiscriminant() < 0)
 	      return 0;
 	    else {
-	      return (-b - getDiscriminant()) / (2 * a);
+	      return (-b - getDiscriminant()) / (2 * a);// root equation r2
 	}
 
 }
